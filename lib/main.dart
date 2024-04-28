cf280c41dd8fbab2a863256bd0c90ee413adafed
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-04-28 10:35:56 -0300

    refactor: app

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Place> _favoritePlaces = [];
+  final List<Place> _favoritePlaces = [];
 
   void _toggleFavorite(Place place) {
     setState(() {

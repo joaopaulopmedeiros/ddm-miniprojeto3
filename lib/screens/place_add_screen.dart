36c4fc694387f42536e421a5b3335db3829fc0e3
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-04-28 13:07:48 -0300

    feat: dropwdown with countries

--- lib/screens/place_add_screen.dart
+++ lib/screens/place_add_screen.dart
@@ -1,5 +1,7 @@
 import 'package:f03_lugares/components/main_drawer.dart';
+import 'package:f03_lugares/providers/country.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AddPlaceScreen extends StatelessWidget {
   const AddPlaceScreen({super.key});
@@ -10,10 +12,41 @@ class AddPlaceScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Adicionar Lugar'),
       ),
-      body: const Center(
-        child: Text('Formulário de Cadastro de Lugares'),
-      ),
+      body: const AddPlaceForm(),
       drawer: const MainDrawer(),
     );
+  }
+}
+
+class AddPlaceForm extends StatefulWidget {
+  const AddPlaceForm({super.key});
+  
+  @override
+  State<StatefulWidget> createState() => _AddPlaceFormState();
+}
+
+class _AddPlaceFormState extends State<AddPlaceForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<CountryProvider>(context);
+    final List<String> _categories = provider.countries.map((e) =>  e.id).toList();
+    String? _selectedCategory;
+
+    return Form(key: _formKey, child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,      
+      children: [
+        DropdownButtonFormField<String>(
+          value: _selectedCategory,
+          items: _categories.map((category) => 
+             DropdownMenuItem<String>(
+                value: category,
+                child: Text(category),
+              )
+          ).toList(), 
+          onChanged: (String? value) {  },
+        ),
+      ],
+    ));
   }
 }

f92da6a448eac848afa3c583c050382347b4ca2f
joaopaulopmedeiros <joaopaulopmedeiros@gmail.com>
2024-04-28 22:41:47 -0300

    feat: selected color

--- lib/screens/country_management_screen.dart
+++ lib/screens/country_management_screen.dart
@@ -2,6 +2,7 @@ import 'package:f03_lugares/components/main_drawer.dart';
 import 'package:f03_lugares/models/country.dart';
 import 'package:f03_lugares/providers/country.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:provider/provider.dart';
 
 class CountryManagementScreen extends StatelessWidget {
@@ -48,7 +49,7 @@ class CountryManagementScreen extends StatelessWidget {
             onPressed: () async {
               final newCountry = await showModalBottomSheet<Country>(
                 context: context,
-                builder: (context) => CreateCountryModal(),
+                builder: (context) => const CreateCountryModal(),
               );
               if (newCountry != null) {
                 countryProvider.addCountry(newCountry);
@@ -60,12 +61,50 @@ class CountryManagementScreen extends StatelessWidget {
   }
 }
 
-class CreateCountryModal extends StatelessWidget {
+class CreateCountryModal extends StatefulWidget {
+  const CreateCountryModal({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CreateCountryModalState();
+}
+
+class _CreateCountryModalState extends State<CreateCountryModal> 
+{
   final TextEditingController idController = TextEditingController();
   final TextEditingController titleController = TextEditingController();
+  Color selectedColor = Colors.blue;
 
   @override
   Widget build(BuildContext context) {
+    void showColorPicker() {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Selecione uma cor'),
+            content: SingleChildScrollView(
+              child: BlockPicker(
+                pickerColor: selectedColor,
+                onColorChanged: (color) {
+                  setState(() {
+                    selectedColor = color;
+                  });
+                },
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -97,6 +136,27 @@ class CreateCountryModal extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 20.0),
+            Row(
+              children: <Widget>[
+                const Text('Cor: '),
+                const SizedBox(width: 10.0),
+                InkWell(
+                  onTap: () {
+                    showColorPicker();
+                  },
+                  child: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: selectedColor,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: () {
                 Navigator.pop(
@@ -104,6 +164,7 @@ class CreateCountryModal extends StatelessWidget {
                   Country(
                     id: idController.text,
                     title: titleController.text,
+                    color: selectedColor
                   ),
                 );
               },
@@ -114,4 +175,5 @@ class CreateCountryModal extends StatelessWidget {
       ),
     );
   }
+  
 }
